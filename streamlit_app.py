df0c4dc3a8f392ee95ccd1c3ab0796a2213c686a
TesterSnow <108015205+TesterSnow@users.noreply.github.com>
2022-06-23 19:12:07 +0530

    Update streamlit_app.py
    Updated New content like header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title("My parents new healthy diner")
+streamlit.title("My Parents New Healthy Diner")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Spinach, Cale and Rocket Smootie")
+streamlit.text("Hard-Boiled Free-Range Eggs")
