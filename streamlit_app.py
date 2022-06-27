345ff3a8b345c36e24fde0336c3c85a2f0c0f0bf
TesterSnow <108015205+TesterSnow@users.noreply.github.com>
2022-06-27 20:30:04 +0530

    Update streamlit_app.py
    Separating the url to have fruit name as param

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 streamlit.header("Fruityvice Fruit Advice!")
 
 #Normalize the JSON response
