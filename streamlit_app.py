23f414272955e3eba855916ba622c80bec6295ab
TesterSnow <108015205+TesterSnow@users.noreply.github.com>
2022-06-28 21:35:04 +0530

    Update streamlit_app.py
    commented streamlit stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ if streamlit.button("Get the fruit list"):
    my_data_rows = get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
+#streamlit.stop()
 #Allowing end user to select a fruit
 
 def insert_row_snowflake(new_fruit):
