bc43765cccf80c489e9e1ba9f414016942b500b9
TesterSnow <108015205+TesterSnow@users.noreply.github.com>
2022-06-28 21:14:55 +0530

    Update streamlit_app.py
    Introduced urlerror and try expect

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,18 +27,18 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-#getting the furit name to be passed to fruityvice api
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-#Normalize the JSON response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#show it in a dataframe table and column
-streamlit.dataframe(fruityvice_normalized)
-
-streamlit.stop();
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get the information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+expect URLError as e:
+  streamlit.error()
+  
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
